4fd461bbea9cb8e326acd250b4fd2aaf02f4c6dc
Anup Deshpande <deshpande.anup24@gmail.com>
2019-04-15 01:53:56 -0400

    change output format of DynamicProgramming, Add dictionary

--- DynamicProgrammingCoinChange.py
+++ DynamicProgrammingCoinChange.py
@@ -13,14 +13,25 @@ def findMinimumCoins(coinValueList,N,minCoins,coinsUsed):
 
 def printChange(coinsUsed,N):
    coin = N
+   dict={}
 
    while coin > 0:
-      thisCoin = coinsUsed[coin]
-      print(thisCoin)
-      coin = coin - thisCoin
+       thisCoin = coinsUsed[coin]
+       if thisCoin in dict.keys():
+           dict[thisCoin]=dict.get(thisCoin)+1
+       else:
+           dict.update({thisCoin: 1})
+       coin = coin - thisCoin
+
+   for i in dict.keys():
+       if dict.get(i) != 0:
+           if dict[i] == 1:
+               print(dict[i], "coin of denomination",i)
+           else:
+               print(dict[i], "coins of denomination",i)
 
 if __name__== '__main__':
-    try:
+   # try:
         N = int(input("Enter value to make change : "))
         print("Making change for",N,"requires")
         denominations = [1, 5, 10, 25]
@@ -30,6 +41,5 @@ if __name__== '__main__':
         findMinimumCoins(denominations, N, coinCount, coinsUsed)
         printChange(coinsUsed, N)
 
-
-    except Exception as e:
-        print("Please enter valid input")
+#    except Exception as e:
+ #       print("Please enter valid input")
